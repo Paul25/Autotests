1fcb652f27747e01f37fa84bfbab6619789ceaa6
Paul25 <pavel.marchenko.6583@gmail.com>
2016-09-01 13:53:54 +0300

    25.03

--- src/test/java/ForPractice.java
+++ src/test/java/ForPractice.java
@@ -1,30 +1,142 @@
+import org.junit.Test;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Marchenko on 29.02.2016.
  */
 public class ForPractice {
+    WebDriver driverBaikonur;
 
+    public static void main(String[] args) throws InterruptedException {
 
-
-
-    public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "C:/Users/Marchenko/Documents/My Received Files/WorkDoc/Installed programms/My Settings/chromedriver_win32/chromedriver.exe");
+        WebDriver driverBaikonur = new ChromeDriver();
+        driverBaikonur.manage().window().maximize();
+        driverBaikonur.get("http://192.168.2.211:8080/baikonur/");
+        Thread.sleep(10000);
+        checkBuildNumberIsPresent(driverBaikonur);
+        incorectLoginName(driverBaikonur);
+        loginToBaikonur(driverBaikonur);
+         // instrumentTable(driverBaikonur);
+        logoutFromBaikonur(driverBaikonur);
+
+       /* System.setProperty("webdriver.chrome.driver", "C:/Users/Marchenko/Documents/My Received Files/WorkDoc/Installed programms/My Settings/chromedriver_win32/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://mail.ru/");
-        driver.findElement(By.xpath("//input[@id='mailbox__login']")).sendKeys("p.a.marchenko");
-        Select dropdown = new Select(driver.findElement(By.id("mailbox__login__domain")));
+        loginToMail(driver);
+        deleteMail(driver);
+        forLogoutAndClose(driver);*/
+    }
 
-        dropdown.selectByVisibleText("@inbox.ru");
+    public static void instrumentTable(WebDriver driverBaikonur) {
 
-        driver.findElement(By.id("mailbox__password")).sendKeys("VjqNtcn1");
-        driver.findElement(By.id("mailbox__auth__button")).click();
+        driverBaikonur.findElement(By.xpath("//div[@id='instrumentsMenuButton']//span[@class='valo-menu-item-caption']")).click();
+        driverBaikonur.findElement(By.xpath("//div[@id='instrumentView']//input[@class='v-textfield v-widget gridcellfilter v-textfield-gridcellfilter tiny v-textfield-tiny v-has-width'][1]")).sendKeys("FOREX");
+    }
+
+    public static void checkBuildNumberIsPresent(WebDriver driverBaikonur) {
+        String buildReal = "1.12.1";
+        WebElement buildNumber = driverBaikonur.findElement(By.xpath("//div[contains(text(),'Build: 1.12.1:[384:e8fa2f7]:2')]"));
+        String buildNumberText = buildNumber.getText();
+
+        System.out.println(buildNumberText);
+        if (buildNumber.isDisplayed()) {
+            if (buildNumberText.contains(buildReal)) {
+                System.out.print("Build number is present");
+                System.out.println(" and it is " + buildReal);
+            } else System.out.println("Build number is not correct");
+        } else System.out.println("Build number is absent");
 
     }
+
+    public static void logoutFromBaikonur(WebDriver driverBaikonur) {
+        driverBaikonur.findElement(By.xpath("//div[@id='settingsMenuBar']//span[@class='v-menubar-menuitem-caption']")).click();
+        driverBaikonur.findElement(By.xpath("//div[@id='baikonur-2001441315-overlays']//span[@class='v-menubar-menuitem-caption']")).click();
+        driverBaikonur.quit();
+    }
+
+    public static void loginToBaikonur(WebDriver driverBaikonur) throws InterruptedException {
+        driverBaikonur.findElement(By.id("usernameInputField")).clear();
+        driverBaikonur.findElement(By.id("passwordInputField")).clear();
+        driverBaikonur.findElement(By.id("usernameInputField")).sendKeys("dealer1");
+        driverBaikonur.findElement(By.id("passwordInputField")).sendKeys("dealer1");
+        driverBaikonur.findElement(By.id("signinButton")).click();
+        Thread.sleep(10000);
+    }
+
+    public static void incorectLoginName(WebDriver driverBaikonur) throws InterruptedException {
+        driverBaikonur.findElement(By.id("usernameInputField")).clear();
+        driverBaikonur.findElement(By.id("passwordInputField")).clear();
+        driverBaikonur.findElement(By.id("usernameInputField")).sendKeys("dealer");
+        driverBaikonur.findElement(By.id("passwordInputField")).sendKeys("dealer1");
+        driverBaikonur.findElement(By.id("signinButton")).click();
+        waitForConditions();
+        Thread.sleep(10000);
+    }
+
+    public static void waitForConditions() throws InterruptedException {
+        WebElement alert;
+        WebDriver driverBaikonur=null;
+        alert = driverBaikonur.findElement(By.xpath("//div[@id='baikonur-2001441315-overlays']//div[@class='v-Notification error v-Notification-error']"));
+        // WebDriverWait waitAlert = new WebDriverWait(driverBaikonur, 10000, 10000);
+        if (alert.isDisplayed()) {
+            WebDriverWait waitAlert = (WebDriverWait) ExpectedConditions.visibilityOfElementLocated((By) alert);
+            Thread.sleep(10000);
+        }else
+            loginToBaikonur(driverBaikonur);
+    }
+
+
+
+/*
+
+    public static void forLogoutAndClose(WebDriver driver) {
+driverBaikonur.findElement(By.id("PH_logoutLink")).click();
+        */
+/*driver.close();*//*
+
+    }
+
+    public static void deleteMail(WebDriver driver) throws InterruptedException {
+        driver.findElement(By.xpath("//div[@class='b-nav__item js-href b-nav__item_active b-nav__item_unread']")).click();
+        Thread.sleep(3000);
+        WebElement mailCheckBox = driver.findElement(By.xpath("//div[@class='b-datalist__body']//div[@class='b-datalist__item js-datalist-item b-datalist__item_unread'][1]"));
+        List<WebElement> elements = new ArrayList<WebElement>();
+        for (int i = 0; i < 10000; i++) {
+            elements.add(mailCheckBox);
+        }
+        if (!elements.isEmpty()) {
+
+            driver.findElement(By.xpath("//div[@id='b-toolbar__right']//div[@class='b-checkbox__box']")).click();
+            driver.findElement(By.xpath("//div[@id='b-toolbar__right']//div[@class='b-toolbar__btn b-toolbar__btn_false b-toolbar__btn_false b-toolbar__btn_adaptive b-toolbar__btn_adaptive_msgl-high b-toolbar__btn_grouped b-toolbar__btn_grouped_first js-shortcut']")).click();
+            Thread.sleep(5000);
+        } else forLogoutAndClose(driver);
+    }
+
+    public static void loginToMail(WebDriver driver) throws InterruptedException {
+        driver.findElement(By.xpath("//input[@id='mailbox__login']")).sendKeys("p.a.marchenko");
+        Select dropdown = new Select(driver.findElement(By.id("mailbox__login__domain")));
+        dropdown.selectByVisibleText("@inbox.ru");
+        driver.findElement(By.id("mailbox__password")).sendKeys("VjqNtcn1");
+        driver.findElement(By.id("mailbox__auth__button")).click();
+        Thread.sleep(10000);
+    }
+*/
+
 }
+
+
--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -11,11 +11,16 @@ import org.junit.runners.JUnit4;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 
 public class MyTest {
 
 
-    public static String login(WebDriver driver, String Username,String password){
+   /* public static String login(WebDriver driver, String Username,String password){
         driver.findElement(By.id("gwt-uid-3")).sendKeys("dealer1");
         driver.findElement(By.id("gwt-uid-5")).sendKeys("dealer1");
         WebElement logIn = driver.findElement(By.xpath("//div[ contains(@class, 'v-button')]"));
@@ -23,10 +28,12 @@ public class MyTest {
 
         String result=driver.findElement(By.id("gwt-uid-5")).getCssValue("");
         return result;
-    }
+    }*/
 
-    public static void main(String[] args) throws InterruptedException {
-        System.setProperty("webdriver.chrome.driver", "C:/Users/Marchenko/Documents/My Received Files/WorkDoc/Installed programms/My Settings/chromedriver_win32/chromedriver.exe");
+    public static void main(String[] args)  {
+        //    throws InterruptedException
+
+        /*System.setProperty("webdriver.chrome.driver", "C:/Users/Marchenko/Documents/My Received Files/WorkDoc/Installed programms/My Settings/chromedriver_win32/chromedriver.exe");
 
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
@@ -34,17 +41,34 @@ public class MyTest {
         compare(login(driver, "dealer1","dealer1"),"dealer1");
         System.out.println(login(driver,"dealer1","dealer1"));
         Thread.sleep(5000);
-        driver.quit();
+        driver.quit();*/
+        //String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(Calendar.getInstance().getTime());
+
+
+
+       /* DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        Date date = new Date();
+        System.out.println(dateFormat.format(date));*/
+
+        long timeMs=System.currentTimeMillis();
+        Date current=new Date();
+        System.out.println(current);
+
+        String test="Hello, my dear frienD.";
+        String test1="Hello, my dear friend.";
+        test.equals(test1);
+        System.out.println(test.compareTo(test1));
+
 
 
     }
-    public static void compare(String real, String unreal) {
+  /*  public static void compare(String real, String unreal) {
         if (real == unreal) {
             System.out.println("Test Passed");
         } else {
             System.out.println("Test failed");
         }
-    }
+    }*/
 
 }
 
